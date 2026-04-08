b27c0a0f5e3f1bb8c0f0067c81b53f1168205bad
Md. AL MUHEETU <almuheetu@gmail.com>
2026-04-08 11:59:54 +0600

    Add UserProfileManager to manage user profile data

--- app/src/main/java/com/shihab/globalprofileapp/data/UserProfileManager.kt
+++ app/src/main/java/com/shihab/globalprofileapp/data/UserProfileManager.kt
@@ -1,2 +1,18 @@
 package com.shihab.globalprofileapp.data
 
+import androidx.compose.runtime.mutableStateOf
+
+object UserProfileManager {
+    var userName = mutableStateOf("")
+    var userRole = mutableStateOf("")
+
+    fun updateProfile(newName: String, newRole: String) {
+        userName.value = newName
+        userRole.value = newRole
+    }
+
+    fun clearData() {
+        userName.value = ""
+        userRole.value = ""
+    }
+}
